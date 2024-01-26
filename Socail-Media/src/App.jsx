2e06e53a77166d1479c5e3d25e03eb9206ca7279
AmreshRajvinderSingh <144646953+AmreshRajvinderSingh@users.noreply.github.com>
2024-01-26 15:47:23 -0500

    added basic Ui

---
+++ Socail-Media/src/App.jsx
@@ -0,0 +1,34 @@
+import { useState } from 'react'
+import "bootstrap/dist/css/bootstrap.min.css";
+import './App.css'
+import Header from './components/Header';
+import Footer from './components/Footer';
+import Sidebar from './components/Sidebar';
+import Create from './components/CreatePost';
+import PostList from './components/PostList';
+import PostListProvider from './store/post-list-store';
+
+function App() {
+  
+
+  const [selected, setSelected]= useState("Home");
+
+  return (
+   <PostListProvider>
+    <div className='app-container'>
+      <Sidebar selected={selected} setSelected={setSelected}/>
+      <div className='content'>
+      <Header />
+      {selected==="Home" ?(<PostList />) :(<Create/>)}
+        
+      
+      <Footer />
+      </div>
+      </div>
+      </PostListProvider>
+      
+  
+  )
+}
+
+export default App
